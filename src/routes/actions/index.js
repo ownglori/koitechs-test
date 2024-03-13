010186cdc3be4b4d96fd12d7afa8487139d86257
Moraiene <vladislav.ponamaryov@gmail.com>
2024-03-13 05:16:46 +0200

    actions

---
+++ src/routes/actions/index.js
@@ -0,0 +1,9 @@
+import {redirect} from "react-router-dom";
+
+
+export const appAction = async ({request}) => {
+  const formData = await request.formData();
+  const user = formData.get("user");
+
+  return redirect(user);
+};

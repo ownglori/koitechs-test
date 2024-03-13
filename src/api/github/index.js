27f4dbc82879d69036c39c77f45fe1a6d48a346d
Moraiene <vladislav.ponamaryov@gmail.com>
2024-03-13 04:16:44 +0200

    github api

---
+++ src/api/github/index.js
@@ -0,0 +1,26 @@
+import {Octokit} from "@octokit/core";
+
+
+const octokit = new Octokit({
+  auth: import.meta.env.GITHUB_AUTH
+});
+
+export const getUser = async (userName) => {
+  try {
+    const response = await octokit.request(`GET /users/${userName}`);
+
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getRepos = async (userName) => {
+  try {
+    const response = await octokit.request(`GET /users/${userName}/repos?per_page=100&sort=pushed`);
+
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};

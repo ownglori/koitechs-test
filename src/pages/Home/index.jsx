40859bcecd60db06b919560b96041afa97798cdb
Moraiene <vladislav.ponamaryov@gmail.com>
2024-03-13 07:20:37 +0200

    home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,9 +3,21 @@ import {Form} from "react-router-dom";
 
 export const Home = () => {
   return (
-    <Form method="post" action="/">
-      <input type="text" name={"user"}/>
-      <button type={"submit"}>submit</button>
-    </Form>
+    <div className="p-4 lg:p-16 w-full flex justify-center items-center">
+      <div className="bg-white max-w-screen-md w-full py-16 px-4 lg:px-16">
+        <div className="mb-4">
+          <h1 className="text-2xl font-bold text-center pb-3">GitHub Resume</h1>
+          <p className="text-sm text-center">Enter your Github username and get your resume.</p>
+        </div>
+        <Form method="post" action="/">
+          <input type="text" name="user" placeholder="GitHub username" required
+                 className="block w-full rounded-md border-0 py-1.5 px-7 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black text-sm leading-6 outline-0 mb-3"/>
+          <button type="submit"
+                  className="w-full text-white bg-black focus:ring-2 focus:ring-black font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">
+            Submit
+          </button>
+        </Form>
+      </div>
+    </div>
   );
 };
